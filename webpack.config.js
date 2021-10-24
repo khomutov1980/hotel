0078ae7a87b929fecb8f138bbfa6b667ada109ba
khomutov1980 <khomutov.main@gmail.com>
2021-10-24 20:40:32 +0300

    install simple-pug-loader, create index.pug and testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: "development",
     entry: './src/app.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: "./index.pug"
+        }),
+      ],
+      module: {
+        rules: [
+          {
+            test: /\.pug$/,
+            use: [
+              {
+                loader: "simple-pug-loader",
+              },
+            ],
+          },
+        ],
+      }
 };
